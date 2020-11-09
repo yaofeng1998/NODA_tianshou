87f780d16fd4804ef98d32aab8e8d5b0d9cfb306
Yao <y-feng20@mails.tsinghua.edu.cn>
2020-11-09 20:57:50 +0800

    change to pixel-wise task

--- hnn/data.py
+++ hnn/data.py
@@ -1,77 +1,159 @@
 # Hamiltonian Neural Networks | 2019
 # Sam Greydanus, Misko Dzamba, Jason Yosinski
 
-import autograd
-import autograd.numpy as np
+import numpy as np
+import gym
+import scipy, scipy.misc
 
-import scipy.integrate
-solve_ivp = scipy.integrate.solve_ivp
+import os, sys
+parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(parent_dir)
 
-def hamiltonian_fn(coords):
-    q, p = np.split(coords,2)
-    H = 3*(1-np.cos(q)) + p**2 # pendulum hamiltonian
-    return H
+from utils import to_pickle, from_pickle
 
-def dynamics_fn(t, coords):
-    dcoords = autograd.grad(hamiltonian_fn)(coords)
-    dqdt, dpdt = np.split(dcoords,2)
-    S = np.concatenate([dpdt, -dqdt], axis=-1)
-    return S
-
-def get_trajectory(t_span=[0,3], timescale=15, radius=None, y0=None, noise_std=0.1, **kwargs):
-    t_eval = np.linspace(t_span[0], t_span[1], int(timescale*(t_span[1]-t_span[0])))
+def get_theta(obs):
+    '''Transforms coordinate basis from the defaults of the gym pendulum env.'''
+    theta = np.arctan2(obs[0], -obs[1])
+    theta = theta + np.pi/2
+    theta = theta + 2*np.pi if theta < -np.pi else theta
+    theta = theta - 2*np.pi if theta > np.pi else theta
+    return theta
     
-    # get initial state
-    if y0 is None:
-        y0 = np.random.rand(2)*2.-1
-    if radius is None:
-        radius = np.random.rand() + 1.3 # sample a range of radii
-    y0 = y0 / np.sqrt((y0**2).sum()) * radius ## set the appropriate radius
+# def preproc(X, side):
+#     '''Crops, downsamples, desaturates, etc. the rgb pendulum observation.'''
+#     X = X[...,0][240:-120,120:-120] - X[...,1][240:-120,120:-120]
+#     return scipy.misc.imresize(X, [int(side/2), side]) / 255.
 
-    spring_ivp = solve_ivp(fun=dynamics_fn, t_span=t_span, y0=y0, t_eval=t_eval, rtol=1e-10, **kwargs)
-    q, p = spring_ivp['y'][0], spring_ivp['y'][1]
-    dydt = [dynamics_fn(None, y) for y in spring_ivp['y'].T]
-    dydt = np.stack(dydt).T
-    dqdt, dpdt = np.split(dydt,2)
+
+def preproc(X, side):
+    '''Crops, downsamples, desaturates, etc. the rgb pendulum observation.'''
+    X = X[...,0][440:-220,330:-330] - X[...,1][440:-220,330:-330]
+    return scipy.misc.imresize(X, [int(side), side]) / 255.
+
+def sample_gym(seed=0, timesteps=103, trials=200, side=28, min_angle=0., max_angle=np.pi/6, 
+              verbose=False, env_name='Pendulum-v0'):
+
+    gym_settings = locals()
+    if verbose:
+        print("Making a dataset of pendulum pixel observations.")
+        print("Edit 5/20/19: you may have to rewrite the `preproc` function depending on your screen size.")
+    env = gym.make(env_name)
+    env.reset() ; env.seed(seed)
+
+    canonical_coords, frames = [], []
+    for step in range(trials*timesteps):
+
+        if step % timesteps == 0:
+            angle_ok = False
+
+            while not angle_ok:
+                obs = env.reset()
+                theta_init = np.abs(get_theta(obs))
+                if verbose:
+                    print("\tCalled reset. Max angle= {:.3f}".format(theta_init))
+                if theta_init > min_angle and theta_init < max_angle:
+                    angle_ok = True
+                  
+            if verbose:
+                print("\tRunning environment...")
+                
+        frames.append(preproc(env.render('rgb_array'), side))
+        obs, _, _, _ = env.step([0.])
+        theta, dtheta = get_theta(obs), obs[-1]
+
+        # The constant factor of 0.25 comes from saying plotting H = PE + KE*c
+        # and choosing c such that total energy is as close to constant as
+        # possible. It's not perfect, but the best we can do.
+        canonical_coords.append( np.array([theta, 0.25 * dtheta]) )
     
-    # add noise
-    q += np.random.randn(*q.shape)*noise_std
-    p += np.random.randn(*p.shape)*noise_std
-    return q, p, dqdt, dpdt, t_eval
+    canonical_coords = np.stack(canonical_coords).reshape(trials*timesteps, -1)
+    frames = np.stack(frames).reshape(trials*timesteps, -1)
+    return canonical_coords, frames, gym_settings
 
-def get_dataset(seed=0, samples=50, test_split=0.5, **kwargs):
-    data = {'meta': locals()}
+def make_gym_dataset(test_split=0.2, **kwargs):
+    '''Constructs a dataset of observations from an OpenAI Gym env'''
+    canonical_coords, frames, gym_settings = sample_gym(**kwargs)
+    
+    coords, dcoords = [], [] # position and velocity data (canonical coordinates)
+    pixels, dpixels = [], [] # position and velocity data (pixel space)
+    next_pixels, next_dpixels = [], [] # (pixel space measurements, 1 timestep in future)
 
-    # randomly sample inputs
-    np.random.seed(seed)
-    xs, dxs = [], []
-    for s in range(samples):
-        x, y, dx, dy, t = get_trajectory(**kwargs)
-        xs.append( np.stack( [x, y]).T )
-        dxs.append( np.stack( [dx, dy]).T )
+    trials = gym_settings['trials']
+    for cc, pix in zip(np.split(canonical_coords, trials), np.split(frames, trials)):
+        # calculate cc offsets
+        cc = cc[1:]
+        dcc = cc[1:] - cc[:-1]
+        cc = cc[1:]
+
+        # concat adjacent frames to get velocity information
+        # now the pixel arrays have same information as canonical coords
+        # ...but in a different (highly nonlinear) basis
+        p = np.concatenate([pix[:-1], pix[1:]], axis=-1)
         
-    data['x'] = np.concatenate(xs)
-    data['dx'] = np.concatenate(dxs).squeeze()
+        dp = p[1:] - p[:-1]
+        p = p[1:]
+
+        # calculate the same quantities, one timestep in the future
+        next_p, next_dp = p[1:], dp[1:]
+        p, dp = p[:-1], dp[:-1]
+        cc, dcc = cc[:-1], dcc[:-1]
+
+        # append to lists
+        coords.append(cc) ; dcoords.append(dcc)
+        pixels.append(p) ; dpixels.append(dp)
+        next_pixels.append(next_p) ; next_dpixels.append(next_dp)
+
+    # concatenate across trials
+    data = {'coords': coords, 'dcoords': dcoords,
+            'pixels': pixels, 'dpixels': dpixels, 
+            'next_pixels': next_pixels, 'next_dpixels': next_dpixels}
+    data = {k: np.concatenate(v) for k, v in data.items()}
 
     # make a train/test split
-    split_ix = int(len(data['x']) * test_split)
+    split_ix = int(data['coords'].shape[0]* test_split)
     split_data = {}
-    for k in ['x', 'dx']:
-        split_data[k], split_data['test_' + k] = data[k][:split_ix], data[k][split_ix:]
+    for k, v in data.items():
+      split_data[k], split_data['test_' + k] = v[split_ix:], v[:split_ix]
     data = split_data
+
+    gym_settings['timesteps'] -= 3 # from all the offsets computed above
+    data['meta'] = gym_settings
+
     return data
 
-def get_field(xmin=-1.2, xmax=1.2, ymin=-1.2, ymax=1.2, gridsize=20):
-    field = {'meta': locals()}
+def get_dataset(experiment_name, save_dir, **kwargs):
+  '''Returns a dataset bult on top of OpenAI Gym observations. Also constructs
+  the dataset if no saved version is available.'''
+  
+  if experiment_name == "pendulum":
+    env_name = "Pendulum-v0"
+  elif experiment_name == "acrobot":
+    env_name = "Acrobot-v1"
+  else:
+    assert experiment_name in ['pendulum']
 
-    # meshgrid to get vector field
-    b, a = np.meshgrid(np.linspace(xmin, xmax, gridsize), np.linspace(ymin, ymax, gridsize))
-    ys = np.stack([b.flatten(), a.flatten()])
-    
-    # get vector directions
-    dydt = [dynamics_fn(None, y) for y in ys.T]
-    dydt = np.stack(dydt).T
+  path = '{}/{}-pixels-dataset.pkl'.format(save_dir, experiment_name)
 
-    field['x'] = ys.T
-    field['dx'] = dydt.T
-    return field
+  try:
+      data = from_pickle(path)
+      print("Successfully loaded data from {}".format(path))
+  except:
+      print("Had a problem loading data from {}. Rebuilding dataset...".format(path))
+      data = make_gym_dataset(**kwargs)
+      to_pickle(data, path)
+
+  return data
+
+
+### FOR DYNAMICS IN ANALYSIS SECTION ###
+def hamiltonian_fn(coords):
+  k = 1.9  # this coefficient must be fit to the data
+  q, p = np.split(coords,2)
+  H = k*(1-np.cos(q)) + p**2 # pendulum hamiltonian
+  return H
+
+def dynamics_fn(t, coords):
+  dcoords = autograd.grad(hamiltonian_fn)(coords)
+  dqdt, dpdt = np.split(dcoords,2)
+  S = -np.concatenate([dpdt, -dqdt], axis=-1)
+  return S
--- hnn/train.py
+++ hnn/train.py
@@ -1,36 +1,69 @@
 # Hamiltonian Neural Networks | 2019
 # Sam Greydanus, Misko Dzamba, Jason Yosinski
 
+import autograd
+import autograd.numpy as np
+import scipy.integrate
+solve_ivp = scipy.integrate.solve_ivp
+
 import torch, argparse
-import numpy as np
 
 import os, sys
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 PARENT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(PARENT_DIR)
 
-from nn_models import MLP
-from hnn import HNN
+from nn_models import MLPAutoencoder, MLP
+from hnn import HNN, PixelHNN
 from data import get_dataset
-from utils import L2_loss, rk4
+from utils import L2_loss
 
 def get_args():
     parser = argparse.ArgumentParser(description=None)
-    parser.add_argument('--input_dim', default=2, type=int, help='dimensionality of input tensor')
+    parser.add_argument('--input_dim', default=2*28**2, type=int, help='dimensionality of input tensor')
     parser.add_argument('--hidden_dim', default=200, type=int, help='hidden dimension of mlp')
+    parser.add_argument('--latent_dim', default=2, type=int, help='latent dimension of autoencoder')
     parser.add_argument('--learn_rate', default=1e-3, type=float, help='learning rate')
+    parser.add_argument('--input_noise', default=0.0, type=float, help='std of noise added to HNN inputs')
+    parser.add_argument('--batch_size', default=200, type=int, help='batch size')
     parser.add_argument('--nonlinearity', default='tanh', type=str, help='neural net nonlinearity')
-    parser.add_argument('--total_steps', default=2000, type=int, help='number of gradient steps')
+    parser.add_argument('--total_steps', default=10000, type=int, help='number of gradient steps')
     parser.add_argument('--print_every', default=200, type=int, help='number of gradient steps between prints')
-    parser.add_argument('--name', default='pend', type=str, help='only one option right now')
-    parser.add_argument('--baseline', dest='baseline', action='store_true', help='run baseline or experiment?')
-    parser.add_argument('--use_rk4', dest='use_rk4', action='store_true', help='integrate derivative with RK4')
     parser.add_argument('--verbose', dest='verbose', action='store_true', help='verbose?')
-    parser.add_argument('--field_type', default='solenoidal', type=str, help='type of vector field to learn')
+    parser.add_argument('--name', default='pixels', type=str, help='either "real" or "sim" data')
+    parser.add_argument('--baseline', dest='baseline', action='store_true', help='run baseline or experiment?')
     parser.add_argument('--seed', default=0, type=int, help='random seed')
     parser.add_argument('--save_dir', default=THIS_DIR, type=str, help='where to save the trained model')
     parser.set_defaults(feature=True)
     return parser.parse_args()
+
+'''The loss for this model is a bit complicated, so we'll
+    define it in a separate function for clarity.'''
+def pixelhnn_loss(x, x_next, model, return_scalar=True):
+  # encode pixel space -> latent dimension
+  z = model.encode(x)
+  z_next = model.encode(x_next)
+
+  # autoencoder loss
+  x_hat = model.decode(z)
+  ae_loss = ((x - x_hat)**2).mean(1)
+
+  # hnn vector field loss
+  noise = args.input_noise * torch.randn(*z.shape)
+  z_hat_next = z + model.time_derivative(z + noise) # replace with rk4
+  hnn_loss = ((z_next - z_hat_next)**2).mean(1)
+
+  # canonical coordinate loss
+  # -> makes latent space look like (x, v) coordinates
+  w, dw = z.split(1,1)
+  w_next, _ = z_next.split(1,1)
+  cc_loss = ((dw-(w_next - w))**2).mean(1)
+
+  # sum losses and take a gradient step
+  loss = ae_loss + cc_loss + 1e-1 * hnn_loss
+  if return_scalar:
+    return loss.mean()
+  return loss
 
 def train(args):
   # set random seed
@@ -38,49 +71,47 @@ def train(args):
   np.random.seed(args.seed)
 
   # init model and optimizer
+  autoencoder = MLPAutoencoder(args.input_dim, args.hidden_dim, args.latent_dim,
+                               nonlinearity='relu')
+  model = PixelHNN(args.latent_dim, args.hidden_dim,
+                   autoencoder=autoencoder, nonlinearity=args.nonlinearity,
+                   baseline=args.baseline)
   if args.verbose:
     print("Training baseline model:" if args.baseline else "Training HNN model:")
+  optim = torch.optim.Adam(model.parameters(), args.learn_rate, weight_decay=1e-5)
 
-  output_dim = args.input_dim if args.baseline else 2
-  nn_model = MLP(args.input_dim, args.hidden_dim, output_dim, args.nonlinearity)
-  model = HNN(args.input_dim, differentiable_model=nn_model,
-              field_type=args.field_type, baseline=args.baseline)
-  optim = torch.optim.Adam(model.parameters(), args.learn_rate, weight_decay=1e-4)
+  # get dataset
+  data = get_dataset('pendulum', args.save_dir, verbose=True, seed=args.seed)
 
-  # arrange data
-  data = get_dataset(seed=args.seed)
-  x = torch.tensor( data['x'], requires_grad=True, dtype=torch.float32)
-  test_x = torch.tensor( data['test_x'], requires_grad=True, dtype=torch.float32)
-  dxdt = torch.Tensor(data['dx'])
-  test_dxdt = torch.Tensor(data['test_dx'])
+  x = torch.tensor( data['pixels'], dtype=torch.float32)
+  test_x = torch.tensor( data['test_pixels'], dtype=torch.float32)
+  next_x = torch.tensor( data['next_pixels'], dtype=torch.float32)
+  test_next_x = torch.tensor( data['test_next_pixels'], dtype=torch.float32)
 
-  # vanilla train loop
+  # vanilla ae train loop
   stats = {'train_loss': [], 'test_loss': []}
   for step in range(args.total_steps+1):
     
     # train step
-    dxdt_hat = model.rk4_time_derivative(x) if args.use_rk4 else model.time_derivative(x)
-    loss = L2_loss(dxdt, dxdt_hat)
+    ixs = torch.randperm(x.shape[0])[:args.batch_size]
+    loss = pixelhnn_loss(x[ixs], next_x[ixs], model)
     loss.backward() ; optim.step() ; optim.zero_grad()
-    
-    # run test data
-    test_dxdt_hat = model.rk4_time_derivative(test_x) if args.use_rk4 else model.time_derivative(test_x)
-    test_loss = L2_loss(test_dxdt, test_dxdt_hat)
 
-    # logging
     stats['train_loss'].append(loss.item())
-    stats['test_loss'].append(test_loss.item())
     if args.verbose and step % args.print_every == 0:
-      print("step {}, train_loss {:.4e}, test_loss {:.4e}".format(step, loss.item(), test_loss.item()))
+      # run validation
+      test_ixs = torch.randperm(test_x.shape[0])[:args.batch_size]
+      test_loss = pixelhnn_loss(test_x[test_ixs], test_next_x[test_ixs], model)
+      stats['test_loss'].append(test_loss.item())
 
-  train_dxdt_hat = model.time_derivative(x)
-  train_dist = (dxdt - train_dxdt_hat)**2
-  test_dxdt_hat = model.time_derivative(test_x)
-  test_dist = (test_dxdt - test_dxdt_hat)**2
+      print("step {}, train_loss {:.4e}, test_loss {:.4e}"
+        .format(step, loss.item(), test_loss.item()))
+
+  train_dist = pixelhnn_loss(x, next_x, model, return_scalar=False)
+  test_dist = pixelhnn_loss(test_x, test_next_x, model, return_scalar=False)
   print('Final train loss {:.4e} +/- {:.4e}\nFinal test loss {:.4e} +/- {:.4e}'
     .format(train_dist.mean().item(), train_dist.std().item()/np.sqrt(train_dist.shape[0]),
             test_dist.mean().item(), test_dist.std().item()/np.sqrt(test_dist.shape[0])))
-
   return model, stats
 
 if __name__ == "__main__":
@@ -89,7 +120,6 @@ if __name__ == "__main__":
 
     # save
     os.makedirs(args.save_dir) if not os.path.exists(args.save_dir) else None
-    label = '-baseline' if args.baseline else '-hnn'
-    label = '-rk4' + label if args.use_rk4 else label
-    path = '{}/{}{}.tar'.format(args.save_dir, args.name, label)
+    label = 'baseline' if args.baseline else 'hnn'
+    path = '{}/{}-pixels-{}.tar'.format(args.save_dir, args.name, label)
     torch.save(model.state_dict(), path)
